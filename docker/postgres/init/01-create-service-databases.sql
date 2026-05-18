7668188b1a875938ccc1fc398c7fa74347d865ef
hakiminazli <muhammadhakimin.azli@mandrill.com.my>
2026-05-18 16:26:28 +0800

    Clarify shared Postgres setup

--- docker/postgres/init/01-create-service-databases.sql
+++ docker/postgres/init/01-create-service-databases.sql
@@ -1,2 +1,5 @@
+SELECT 'CREATE DATABASE auth_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth_db')\gexec
+
 SELECT 'CREATE DATABASE member_db'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'member_db')\gexec
